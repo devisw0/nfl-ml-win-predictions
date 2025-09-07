8845ce8e86c7979b8df454f791c9d6fa4175d16f
Devan P. <dbp119@scarletmail.rutgers.edu>
2025-09-06 20:06:19 -0400

    1st

---
+++ main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('2017-2025_scores.csv')
+
+df = df.drop(['GameStatus','Day','AwayRecord','AwayScore','HomeRecord',
+              'HomeScore','AwaySeeding',	'HomeSeeding',
+              'PostSeason'], axis = 1)
+
+no_preseason = df[~df['Week'].str.contains('Preseason', case=False, na=False)]
+#case is case sensitivity (true = case sensitive), ~ = not, na = False means we treat Nans as False so in our case they arent removed
+
+season_and_postseason = no_preseason[~no_preseason['Week'].str.contains('Hall', case=False, na=False)]
+#format is the format of our input string, errors=coerce means treat as a Nat, not a time (like Nan)
+season_and_postseason = season_and_postseason.copy()  
+
+season_and_postseason['Date'] = pd.to_datetime(
+    season_and_postseason['Date'].str.strip(),
+    format='%m/%d',
+    errors='coerce'
+)
+
+
+season_and_postseason['Date'] = season_and_postseason['Date'].to_datetime(format = '%m/%d', errors = 'coerce')
+#format is the format of our input string, errors=coerce means treat as a Nat, not a time (like Nan)
+print(season_and_postseason.head())
+
