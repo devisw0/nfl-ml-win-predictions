750aed2ad8492f8621b02a987e34e6d8ed1cdf13
Devan P. <91017674+devisw0@users.noreply.github.com>
2025-09-09 12:13:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,18 +18,28 @@ season_and_postseason['Date'] = pd.to_datetime(
     format='%m/%d',
     errors='coerce'
 )
-#turning our dates into a canonical stirng for datetime accessors etc.
+#turning our dates into a canonical stirng for datetime accessors etc. such that it can put our dates in a way datetime can understand
+#so after this line, we specify the format it is then able to understand all since it changes the way the dates are such that it can understand
 #format is the format of our input string, errors=coerce means treat as a Nat, not a time (like Nan)
 
 season_and_postseason['Month'] = season_and_postseason['Date'].dt.month
 season_and_postseason['Day'] = season_and_postseason['Date'].dt.day
 
-season_and_postseason['Is Jan or Feb'] = season_and_postseason['Month'].isin([1,2]) 
+mask = season_and_postseason['Month'].isin([1,2]) 
 
 season_and_postseason['game year'] = season_and_postseason['Season']
 
-season_and_postseason['game year'] = season_and_postseason.loc['Is Jan or Feb', 'game year'] + 1
+season_and_postseason.loc[mask, 'game_year'] = (
+    season_and_postseason.loc[mask, 'Season'] + 1
+)
 
+game_date = {'year': season_and_postseason['game_year'], 'month': season_and_postseason['Month'], 'day': season_and_postseason['Day'] }
+
+season_and_postseason['game_date'] = pd.to_datetime(game_date, errors = 'coerce')
+
+season_and_postseason = season_and_postseason.dropna(subset = ['game_date'])
+
+season_and_postseason = season_and_postseason.sort_values(by = 'game_date')
 
 print(season_and_postseason.head())
 
